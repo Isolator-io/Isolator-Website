defa92d4372e5f11bc7d47f5cddc03fce1c92d93
brianxautumn <shred@brianautumn.com>
2016-06-06 16:56:24 -0700

    Setup basic landing site

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Isolator</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <canvas></canvas>
+        <div id ="image-wrap"></div>
+        <div class="container-fixed">
+
+            <div class="row vertical-align">
+
+                <div class="col-lg-12 text-center v-center">
+
+                    <h1><i class="fa fa-file-video-o" aria-hidden="true"></i></h1>
+                    <h2>Isolator</h2>
+                    <p class="lead">Process Iso based files</p>
+
+                    <br>
+
+                    <a class="btn btn-primary" href="https://github.com/Isolator-io" role="button">Github</a>
+                    <a class="btn btn-primary" href="http://isolator.io/demo/example/" role="button">Try The Demo</a>
+                </div>
+
+            </div> <!-- /row -->
+
+
+        </div> <!-- /container full -->
+
+
+        <script   src="https://code.jquery.com/jquery-2.2.4.min.js"   integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="   crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="js/constellation.js"></script>
+    </body>
+</html>
